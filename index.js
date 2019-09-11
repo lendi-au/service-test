81ba64cc0f5f18a9ac9bcc57edff223da071b86e
Jordan Simonovski <jordan.simonovski@lendi.com.au>
2019-09-12 09:36:47 +1000

    outputting success message

--- index.js
+++ index.js
@@ -3,4 +3,11 @@ const pgp = require('pg-promise')({});
 const cn = 'postgres://admin:admin@postgres:5432/users';
 const db = pgp(cn);
 
-db.connect();
+db.connect()
+    .then(obj => {
+        obj.done(); // success, release the connection;
+        console.log('Connection successful!');
+    })
+    .catch(error => {
+        console.log('ERROR:', error.message || error);
+    });
