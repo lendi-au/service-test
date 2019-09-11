9d9ba9c6296de2bf390528261465a798af54f9ca
Jordan Simonovski <jordan.simonovski@lendi.com.au>
2019-09-12 09:13:11 +1000

    connect time

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const pgp = require('pg-promise');
+const pgp = require('pg-promise')({});
 
 const cn = 'postgres://admin:admin@host:5432/users';
 const db = pgp(cn);
 
-module.exports = db;
+db.connect();
